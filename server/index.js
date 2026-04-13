26ef3a1c007ccfbcbea5c41700d1169c405da75b
MD RIZWAN KHAN <rizwankhanbara@gmail.com>
2026-04-13 23:34:54 +0530

    Configure MERN app for All-in-One Vercel Deployment

--- server/index.js
+++ server/index.js
@@ -40,8 +40,10 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'production' || !process.env.VERCEL) {
+    app.listen(PORT, () => {
+        console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+    });
+}
 
-app.listen(PORT, () => {
-    console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
-});
+export default app;
